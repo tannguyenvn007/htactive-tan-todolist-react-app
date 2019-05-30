bcacf09fe75907155eaeb62290bb3fc762406bfc
tan.nguyen <tan.nguyen@pn.com.vn>
2019-05-30 20:30:11 +0700

    Updates

---
+++ precache-manifest.2f0ed27a70cd255d872c3b74e959f01d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21841efecd7181057a9521f0683df13d",
+    "url": "/htactive-tan-todolist-react-app/index.html"
+  },
+  {
+    "revision": "e54317a5cd7666c7a9a0",
+    "url": "/htactive-tan-todolist-react-app/static/css/2.5d832d7a.chunk.css"
+  },
+  {
+    "revision": "584f70e8033320ffd9d3",
+    "url": "/htactive-tan-todolist-react-app/static/css/main.01985a65.chunk.css"
+  },
+  {
+    "revision": "e54317a5cd7666c7a9a0",
+    "url": "/htactive-tan-todolist-react-app/static/js/2.cf5b9a26.chunk.js"
+  },
+  {
+    "revision": "584f70e8033320ffd9d3",
+    "url": "/htactive-tan-todolist-react-app/static/js/main.4993fe05.chunk.js"
+  },
+  {
+    "revision": "19d05c4c03f556139c9c",
+    "url": "/htactive-tan-todolist-react-app/static/js/runtime~main.d903908d.js"
+  }
+]);
