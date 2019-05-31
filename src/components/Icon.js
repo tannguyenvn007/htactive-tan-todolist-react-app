1533f057e095d089d1b05eb320ff60999a88ef79
tannguyenvn007 <32976476+tannguyenvn007@users.noreply.github.com>
2019-05-31 13:47:28 +0700

    Update Icon.js

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => {
   return (
-    <a href="/">
+    <a href="https://github.com/tannguyenvn007/htactive-tan-todolist-react-app" target="_blank">
       <img src="./images/github.png" height="23px" alt="" />
     </a>
   );
